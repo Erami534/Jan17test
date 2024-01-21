2b6d5ec1db5e50e26d8becd43dfe9bc9a5afa627
Erami534 <156941787+Erami534@users.noreply.github.com>
2024-01-21 10:32:54 -0500

    Add files via upload

---
+++ Triangleprogram/src/Triangle.java
@@ -0,0 +1,17 @@
+//Chapter 1 Assignment 1, Antonio Ramirez. This outputs a triangle
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("       T ");
+		System.out.println("      TTT ");
+		System.out.println("     TTTTT ");
+		System.out.println("    TTTTTTT ");
+		System.out.println("   TTTTTTTTT ");
+		System.out.println("  TTTTTTTTTTT ");
+		System.out.println(" TTTTTTTTTTTTT ");
+
+	}
+
+}
